874e31f1ba7cc7f387c9b36511a93db4cff97b8e
mohammadumar28 <mohammadumar28@gmail.com>
2020-09-09 21:20:38 +0530

    Project complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :username, presence: true, length: { in: 5..16 }, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
 end
